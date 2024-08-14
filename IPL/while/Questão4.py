18d715bc9108e35929187ea8643b3a20fc9e7029
jotinhaeiperigodeterminado <joao.vieira.017@ufrn.edu.br>
2024-08-14 11:06:19 -0300

    new file:   "IPL/while/Quest\303\243o4.py"

---
+++ IPL/while/Questão4.py
@@ -0,0 +1,18 @@
+#Escreva um programa que gere um número aleatório entre 1 e 100 e peça ao usuário
+#para adivinhar qual é o número. O programa deve continuar pedindo palpites até que
+#o usuário acerte o número. Ao final, o programa deve informar quantos palpites foram
+#necessários e informar que o programa encerrou.
+
+import random
+
+num_ale = random.randint(1, 100)
+print(num_ale)
+
+num_test = int(input('Advinhe o numero: '))
+
+while(num_ale != num_test):
+    num_test = int(input('tente novamente: '))
+
+print('Você acertou')
+
+print('Fim do programa')
