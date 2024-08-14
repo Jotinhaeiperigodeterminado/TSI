e549092bc7838779db2a0f42987051fa1609b3c1
jotinhaeiperigodeterminado <joao.vieira.017@ufrn.edu.br>
2024-08-14 11:01:17 -0300

    modified:   "IPL/while/Quest\303\243o1.py"

--- IPL/while/Questão1.py
+++ IPL/while/Questão1.py
@@ -1,3 +1,13 @@
 #Escreva um programa que exiba os valores dos números de 1 até 100. Ao término o
 #programa deverá exibir uma mensagem de encerramento informando que o programa
 #terminou.
+
+num = 0
+
+botao = input('Digite s para iniciar: ')
+
+if(botao == 's'):
+    while (num < 100):
+        num = num + 1
+        print(num)
+print('Fim do programa.')
